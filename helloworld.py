b6377dd76a7c6e63fb35644a3706a15d7f5a3265
Fanghan Wu <fanghanwu@Fanghans-MacBook-Pro.local>
2018-10-12 23:41:13 -0400

    add helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import datetime
+
+
+# In[2]:
+
+def hello_world():
+    print('hello world!')
+    now = datetime.datetime.now()
+    print('it is {}'.format(now))
+
+if __name__ == "__main__":
+    hello_world()
+
+
+# In[ ]:
+
+
+
